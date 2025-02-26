3cdb0a968e52696a2d9e799514c26ca11695e5c8
witchlet <npttorres@gmail.com>
2025-02-26 21:49:29 +0800

    get money yah yah

--- kinetic-light/sense-interference.js
+++ kinetic-light/sense-interference.js
@@ -1,5 +1,5 @@
 //random background image
 const body = document.querySelector("body");
 var images = ['IMG_5394.jpg', 'IMG_5404.jpg', 'IMG_5412.jpg', 'IMG_5426.jpg'];
-var randomImage = "url(images/kinetic-light/" + images[Math.floor(Math.random() * images.length)] + ")";
+var randomImage = "url(/images/kinetic-light/" + images[Math.floor(Math.random() * images.length)] + ")";
 body.style.backgroundImage = randomImage;
